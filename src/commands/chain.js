d94bd460ebc158c13f44b90dcacfd2c4c157c0d3
Gnong <jinhongdev@gmail.com>
2022-05-16 15:35:24 +0900

    Edit indentation

--- src/commands/chain.js
+++ src/commands/chain.js
@@ -66,9 +66,9 @@ async function main(archwayd, name, options = {}) {
             command = spawn(archwayd.command, [name]);
             break;
         case 'reset':
-	    console.log("Reset block data");
-	    command = spawn(archwayd.command, ["unsafe-reset-all"]);
-	    command.stderr.on('data', (data) => {
+            console.log("Reset block data");
+            command = spawn(archwayd.command, ["unsafe-reset-all"]);
+            command.stderr.on('data', (data) => {
                 process.stdout.write(`${data}`);
             });	
             break;
