d2c0aa5da99f223f4fa4d74cae67c4892ee056be
Gnong <jinhongdev@gmail.com>
2022-05-19 17:44:29 +0900

    Modify the path to a compressed folder

--- src/commands/chain.js
+++ src/commands/chain.js
@@ -93,7 +93,7 @@ async function main(archwayd, name, options = {}) {
             if (mkDir.error != null){
                 throw mkDir.error;
             }
-            const snapshot = spawn("tar", ["zcvf", `${rootPath}/snapshots/chaindata_${nowDate.toISOString()}.tar.gz`,`${archwayd.archwaydHome}/data`, "-C", `${archwayd.archwaydHome}`]);
+            const snapshot = spawn("tar", ["-C", `${archwayd.archwaydHome}`, "-zcvf", `${rootPath}/snapshots/chaindata_${nowDate.toISOString()}.tar.gz`, `data`]);
             snapshot.stdout.on('data', (data) => {
                 process.stdout.write(`${data}`);
                 console.log(`Snapshot creation completed. Location: ${rootPath}/snapshots`);
